34bd5f38a49dcdbf5d106719fccca77893a2efe4
Sevatkaaa <sevatkaaa@gmail.com>
2018-09-23 14:19:40 +0300

    Spring Boot init

---
+++ src/main/java/com/sinners/SinnersJavaApp.java
@@ -0,0 +1,13 @@
+package com.sinners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinnersJavaApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinnersJavaApp.class, args);
+    }
+
+}
